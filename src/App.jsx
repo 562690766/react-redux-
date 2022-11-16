93ea4bac6c47ca25f1daf2e41e7b5b25fbd7a15a
562690766 <562690766@qq.com>
2022-11-17 06:13:53 +0800

    21.9.2.207;

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-//'react'=>'node_modules/react/index.js'
-import React, { Component } from 'react'
-import BaseRouter from '@/router/index'
 
+import React, { Component } from 'react'
+/* import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+import App from './App'
+ */
 class App extends Component {
     render() {
         return (
             <div>
                 <h3>App组件1234</h3>
-                <BaseRouter></BaseRouter>
+                
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import {createStore} from 'redux'
+function reducer(state=0,action){
+    console.log("reducer调用了",action);
+    switch(action.type){
+        case 'ADD':
+            return state+1;
+        case 'SUB':
+            return state-1;
+        defaule:
+            return state;
+    }
+}
+const store =createStore(reducer);
+/* function createStore(reducer){
+    reducer(undefind,{type:'@@redux/INITz.5.q.5.i.o'})
+} */
+console.log("store state",store.getState());
+
 ReactDOM.render(
     <App />,
     document.getElementById("app")
